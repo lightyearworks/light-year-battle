1c09175012113cb7e11e02d68479a17af19a1a5b
lch-howger <light_year_dev@163.com>
2022-01-18 13:26:32 +0800

    Update BattleConfig.sol

--- contracts/config/BattleConfig.sol
+++ contracts/config/BattleConfig.sol
@@ -20,6 +20,22 @@ contract BattleConfig is IBattleConfig {
     function getRealDamage(IBattle.BattleShip memory attacker_, IBattle.BattleShip memory defender_) external override pure returns (uint32){
         uint32 attack = attacker_.attack;
         uint32 defense = defender_.defense;
+        uint256 attackerWeapon = getWeaponType(attacker_);
+        uint256 defenderWeapon = getWeaponType(defender_);
+        if ((attackerWeapon == 3 && defenderWeapon == 2) || (attackerWeapon == 2 && defenderWeapon == 1)) {
+            attack = attack * 120 / 100;
+        }
         return (attack * attack) / (attack + defense);
+    }
+
+    function getWeaponType(IBattle.BattleShip memory ship_) public pure returns (uint256){
+        if (ship_.shipType == 2 || ship_.shipType == 9 || ship_.shipType == 16) {
+            return 1;
+        } else if (ship_.shipType == 3 || ship_.shipType == 10 || ship_.shipType == 17) {
+            return 2;
+        } else if (ship_.shipType == 7 || ship_.shipType == 13 || ship_.shipType == 18) {
+            return 3;
+        }
+        return 0;
     }
 }
