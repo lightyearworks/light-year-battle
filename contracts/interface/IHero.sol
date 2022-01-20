b12096b58934c818b23d87551d4a91b25cfc750a
lch-howger <light_year_dev@163.com>
2022-01-20 13:02:14 +0800

    ship hero interface

--- contracts/interface/IHero.sol
+++ contracts/interface/IHero.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.6.12;
 interface IHero {
 
     struct Info {
-        uint16 level;
+        uint8 level;
         uint8 quality;
         uint8 heroType;
     }
 
-    function operatorTransfer(address from, address to, uint256 tokenId) external;
-    function heroInfo(uint256 shipId) external view returns (Info memory);
-    function upgradeHero(uint256 heroId_) external;
+    function heroInfo(uint256 shipId_) external view returns (Info memory);
+    function upgradeHero(uint256 heroFromTokenId_, uint256 heroToTokenId_) external;
+    function convertHero(uint256 heroTokenId_) external;
 }
--- contracts/interface/IShip.sol
+++ contracts/interface/IShip.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.6.12;
 interface IShip {
 
     struct Info {
-        uint16 level;
+        uint8 level;
         uint8 quality;
         uint8 shipType;
     }
 
-    function operatorTransfer(address from, address to, uint256 tokenId) external;
-    function shipInfo(uint256 shipId) external view returns(Info memory);
+    function shipInfo(uint256 shipId_) external view returns (Info memory);
+    function buildShip(uint8 shipType_) external;
     function upgradeShip(uint256 shipFromTokenId_, uint256 shipToTokenId_) external;
 }
