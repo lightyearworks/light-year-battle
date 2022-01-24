e0a1accf04558264740d40a3340be3b97efb3422
lch-howger <light_year_dev@163.com>
2022-01-24 14:41:09 +0800

    Update IShip.sol

--- contracts/interface/IShip.sol
+++ contracts/interface/IShip.sol
@@ -10,6 +10,8 @@ interface IShip {
         uint8 shipType;
     }
 
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
     function shipInfo(uint256 shipId_) external view returns (Info memory);
     function buildShip(uint8 shipType_) external;
